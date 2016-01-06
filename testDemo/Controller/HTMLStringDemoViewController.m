13d15b18a4cb71afe29c917c5c4778588f349e83
YMY <398411773@qq.com>
2016-01-06 09:56:34 +0800

    #1.0 修改css

--- testDemo/Controller/HTMLStringDemoViewController.m
+++ testDemo/Controller/HTMLStringDemoViewController.m
@@ -24,7 +24,7 @@
     web.delegate = self;
     [self.view addSubview:web];
     
-    NSString *_htmlStr = @"<!DOCTYPE html><html><head lang=\"zh\"><meta charset=\"UTF-8\"><title>第一个HTML</title><link rel=\"stylesheet\" type=\"text/css\" href=\"index.css\"><script type=\"text/javascript\" src=\"index.js\"></script></head><body><h1>我是HTMLString</h1><p id = \"p\">p标签</p><img id = \"img\" src = \"2.pic_hd.jpg\" alt = \"百度LOGO\"><br/><a id = \"a\" href=\"http://baidu.com\">我要到百度</a><br/><br/><br/><button onclick = \"hello()\">点击我弹出hello</button></body></html>";
+    NSString *_htmlStr = @"<!DOCTYPE html><html><head lang=\"zh\"><meta charset=\"UTF-8\"><title>第一个HTML</title><link rel=\"stylesheet\" type=\"text/css\" href=\"index.css\"><script type=\"text/javascript\" src=\"index.js\"></script></head><body><h1>我是HTMLString</h1><p id = \"p\">p标签</p><img id = \"img\" src = \"2.pic_hd.jpg\" alt = \"百度LOGO\"><br/><a id = \"a\" href=\"http://baidu.com\">我要到百度</a><br/><br/><br/><button id = \"btn\" onclick = \"hello()\">点击我弹出hello</button></body></html>";
     NSLog(@"%@",[NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath] isDirectory: YES]);
     [web loadHTMLString:_htmlStr baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath] isDirectory: YES]];
 }
